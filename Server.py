c9f48e9f4da96a982c1c127b6a9d9e112e3f5582
sergiooli1997 <gio.lozol@outlook.com>
2020-05-28 00:56:56 -0500

    V 2.1

--- Server.py
+++ Server.py
@@ -18,45 +18,80 @@ with SimpleXMLRPCServer(('localhost', 8000),
 
     class MyFuncs:
         def create(self, path):
-            final_path = os.path.join(start_path, path)
-            fd = open(final_path, "w+")
-            fd.write("\n")
-            fd.close()
+            try:
+                final_path = os.path.join(start_path, path)
+                fd = open(final_path, "w+")
+                fd.write("\n")
+                fd.close()
+                print('Archivo creado')
+            except OSError:
+                print('No se pudo crear archivo')
 
         def read(self, path):
-            final_path = os.path.join(start_path, path)
-            fd = open(final_path, "r")
-            contents = fd.read()
-            return contents
+            try:
+                final_path = os.path.join(start_path, path)
+                fd = open(final_path, "r")
+                contents = fd.read()
+                print('Archivo leido')
+                return contents
+            except FileNotFoundError:
+                print('No existe archivo solicitado')
 
         def write(self, path, texto):
-            final_path = os.path.join(start_path, path)
-            fd = open(final_path, "a+")
-            fd.write(texto)
-            fd.close()
+            try:
+                final_path = os.path.join(start_path, path)
+                fd = open(final_path, "a+")
+                fd.write(texto)
+                fd.close()
+                print('Se escribio en archivo')
+            except FileNotFoundError:
+                print('No existe archivo solicitado')
 
         def rename(self, path, new_name):
-            final_path = os.path.join(start_path, path)
-            dir = os.path.dirname(final_path)
-            new_dir = os.path.join(dir, new_name)
-            os.rename(final_path, new_dir)
+            try:
+                final_path = os.path.join(start_path, path)
+                dir = os.path.dirname(final_path)
+                new_dir = os.path.join(dir, new_name)
+                os.rename(final_path, new_dir)
+                print('Se renombro archivo')
+            except FileNotFoundError:
+                print('No existe archivo solicitado')
 
         def remove(self, path):
-            final_path = os.path.join(start_path, path)
-            os.remove(final_path)
+            try:
+                final_path = os.path.join(start_path, path)
+                os.remove(final_path)
+                print('Se elimino archivo')
+            except FileNotFoundError:
+                print('No existe archivo solicitado')
 
         def mkdir(self, path):
-            final_path = os.path.join(start_path, path)
-            os.mkdir(final_path, mode=0o777)
+            try:
+                final_path = os.path.join(start_path, path)
+                os.mkdir(final_path, mode=0o777)
+                print('Se creo directorio')
+            except OSError:
+                print('No se pudo crear directorio')
 
         def rmdir(self, path):
-            final_path = os.path.join(start_path, path)
-            os.rmdir(final_path)
+            try:
+                final_path = os.path.join(start_path, path)
+                os.rmdir(final_path)
+                print('Se elimino directorio')
+            except OSError:
+                print('No se pudo eliminar directorio')
 
         def readdir(self, path):
-            final_path = os.path.join(start_path, path)
-            ls = os.listdir(final_path)
-            return ls
+            try:
+                if path == '/':
+                    ls = os.listdir(start_path)
+                else:
+                    final_path = os.path.join(start_path, path)
+                    ls = os.listdir(final_path)
+                print('Se listo directorio')
+                return ls
+            except OSError:
+                print('Error en el directorio')
 
 
     server.register_instance(MyFuncs())
